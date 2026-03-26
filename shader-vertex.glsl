5891e79af9636a71f03b79bb3eb602a99e099a2b
Laszlo Korte <email@laszlokorte.de>
2026-03-27 00:07:10 +0100

    add fullscreen and rotator control

--- shader-vertex.glsl
+++ shader-vertex.glsl
@@ -7,6 +7,8 @@ uniform float sheer;
 uniform vec2 offset;
 uniform float rotation;
 
+uniform float plotPolar;
+
 varying vec2 uvCoord;
 
 vec2 rot(vec2 subject, float rad, vec2 pivot) {
@@ -15,7 +17,8 @@ vec2 rot(vec2 subject, float rad, vec2 pivot) {
 }
 
 void main() {
-    uvCoord = rot(position + vec2(0.0, position.x * sheer) + vec2(offset.x, -offset.y) * 0.5, rotation, vec2(0.5));
+    uvCoord = rot(position + vec2(offset.x, -offset.y) * 0.5, rotation, vec2(0.5));
+    uvCoord += vec2(0.0, (uvCoord.x - 0.5) * mix(sheer, 0.0, plotPolar));
 
     gl_Position = vec4(1.0 - 2.0 * position, 0, 1);
 }
