5e2f944db6eae9442ef2d38da202288ef703e15d
Laszlo Korte <me@laszlokorte.de>
2026-03-28 20:01:32 +0100

    improve loading screen

--- shader-vertex.glsl
+++ shader-vertex.glsl
@@ -20,9 +20,9 @@ vec2 rot(vec2 subject, float rad, vec2 pivot) {
 void main() {
     float shrinkX = max(1.0, aspect);
     float shrinkY = max(1.0, 1.0 / aspect);
-    vec2 correctedAspectRatio = vec2((position.x - 0.5) * shrinkX, (position.y - 0.5) * shrinkY) + 0.5;
+    vec2 correctedAspectRatio = vec2((position.x - 0.5) * shrinkX, (position.y - 0.5) * shrinkY)/ aspect + 0.5;
 
-    uvCoord = rot(correctedAspectRatio + vec2(offset.x, -offset.y) * 0.5, rotation, vec2(0.5));
+    uvCoord = rot(correctedAspectRatio + vec2(offset.x, -offset.y) /aspect * 0.5, rotation, vec2(0.5)) ;
     uvCoord += vec2(0.0, (uvCoord.x - 0.5) * mix(sheer, 0.0, plotPolar));
 
     gl_Position = vec4(1.0 - 2.0 * position, 0, 1);
